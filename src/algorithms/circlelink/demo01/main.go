8975e032c21ee98b8f60482fcf592cd1729cc974
Abelit <ychenid@live.com>
2020-11-09 21:30:54 +0800

    aupdate

--- src/algorithms/circlelink/demo01/main.go
+++ src/algorithms/circlelink/demo01/main.go
@@ -4,47 +4,48 @@ import (
 	"fmt"
 )
 
+//定义猫的结构体结点
 type CatNode struct {
-	no   int
+	no   int //猫猫的编号
 	name string
 	next *CatNode
 }
 
 func InsertCatNode(head *CatNode, newCatNode *CatNode) {
-	// 判断是不是第一次猫
+
+	//判断是不是添加第一只猫
 	if head.next == nil {
 		head.no = newCatNode.no
 		head.name = newCatNode.name
-		head.next = head //构成环形
+		head.next = head //构成一个环形
+		fmt.Println(newCatNode, "加入到环形的链表")
 		return
 	}
 
-	// 定义一个临时变量，找到环形的最后节点
+	//定义一个临时变量，帮忙,找到环形的最后结点
 	temp := head
-
 	for {
 		if temp.next == head {
 			break
 		}
 		temp = temp.next
 	}
-
-	// 加入环形链表
+	//加入到链表中
 	temp.next = newCatNode
 	newCatNode.next = head
 
 }
 
+//输出这个环形的链表
 func ListCircleLink(head *CatNode) {
+	fmt.Println("环形链表的情况如下：")
 	temp := head
-
 	if temp.next == nil {
-		fmt.Println("空链表")
+		fmt.Println("空空如也的环形链表...")
 		return
 	}
-
 	for {
-		fmt.Printf("猫的信息是=%s %s ", temp.name, "->")
+		fmt.Printf("猫的信息为=[id=%d name=%s] ->\n", temp.no, temp.name)
 		if temp.next == head {
 			break
 		}
@@ -52,20 +53,26 @@ func ListCircleLink(head *CatNode) {
 	}
 }
 
-func DelCircleLink(head *CatNode, id int) *CatNode {
+//删除一只猫
+func DelCatNode(head *CatNode, id int) *CatNode {
+
 	temp := head
 	helper := head
+	//空链表
 	if temp.next == nil {
-		fmt.Println("空链表")
+		fmt.Println("这是一个空的环形链表，不能删除")
 		return head
 	}
 
-	// 只有一个节点
-	if temp.next == head {
-		temp.next = nil
+	//如果只有一个结点
+	if temp.next == head { //只有一个结点
+		if temp.no == id {
+			temp.next = nil
+		}
 		return head
 	}
 
+	//将helper 定位到链表最后
 	for {
 		if helper.next == head {
 			break
@@ -73,42 +80,43 @@ func DelCircleLink(head *CatNode, id int) *CatNode {
 		helper = helper.next
 	}
 
+	//如果有两个包含两个以上结点
 	flag := true
-
-	// 多个节点
 	for {
-		if temp.next == head { //还差最后一个需要比较
+		if temp.next == head { //如果到这来，说明我比较到最后一个【最后一个还没比较】
 			break
 		}
-
-		if temp.no == id { //找到了
-			if temp == head { //删除头节点
-
+		if temp.no == id {
+			if temp == head { //说明删除的是头结点
+				head = head.next
 			}
+			//恭喜找到., 我们也可以在直接删除
 			helper.next = temp.next
-			fmt.Println("不存在", id)
+			fmt.Printf("猫猫=%d\n", id)
 			flag = false
 			break
 		}
-		temp = temp.next
-		helper = helper.next
+		temp = temp.next     //移动 【比较】
+		helper = helper.next //移动 【一旦找到要删除的结点 helper】
 	}
-	if flag {
+	//这里还有比较一次
+	if flag { //如果flag 为真，则我们上面没有删除
 		if temp.no == id {
 			helper.next = temp.next
-			fmt.Println("猫猫", id)
+			fmt.Printf("猫猫=%d\n", id)
 		} else {
-			fmt.Println("不存在", id)
+			fmt.Printf("对不起，没有no=%d\n", id)
 		}
 	}
 	return head
 }
 
 func main() {
-	// 初始化环形链表的头节点
+
+	//这里我们初始化一个环形链表的头结点
 	head := &CatNode{}
 
-	// 创建一只猫
+	//创建一只猫
 	cat1 := &CatNode{
 		no:   1,
 		name: "tom",
@@ -117,11 +125,20 @@ func main() {
 		no:   2,
 		name: "tom2",
 	}
+	cat3 := &CatNode{
+		no:   3,
+		name: "tom3",
+	}
 	InsertCatNode(head, cat1)
 	InsertCatNode(head, cat2)
+	InsertCatNode(head, cat3)
 	ListCircleLink(head)
 
-	// DelCircleLink(head, 1)
-	// fmt.Println()
-	// ListCircleLink(head)
+	head = DelCatNode(head, 1)
+
+	fmt.Println()
+	fmt.Println()
+	fmt.Println()
+	ListCircleLink(head)
+
 }
